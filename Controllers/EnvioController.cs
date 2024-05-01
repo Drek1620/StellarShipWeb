44db593143eb443a2827f40c5ac42efe513ee39c
Alan Gerardo <alangera1602@gmail.com>
2024-04-30 18:03:38 -0600

    Cambiossd

---
+++ Controllers/EnvioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StellarShipWeb.Controllers
+{
+    public class EnvioController : Controller
+    {
+        public IActionResult Seguimiento_envio()
+        {
+            return View();
+        }
+    }
+}
--- Models/BusquedaPaquete.cs
+++ Models/BusquedaPaquete.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace StellarShipWeb.Models
 {
     public class BusquedaPaquete
     {
         [Required]
+        [Remote(action: "VerificarExisteTipoCuenta", controller: "TiposCuentas")]
         public long IdGuia { get; set; }
     }
 }
