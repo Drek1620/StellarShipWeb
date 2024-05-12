4318d7d55a1770b0edf105ed63aa17316deb0e77
Alan Gerardo <alangera1602@gmail.com>
2024-05-12 11:52:03 -0600

    sdf

--- Models/BusquedaPaquete.cs
+++ Models/BusquedaPaquete.cs
@@ -5,7 +5,7 @@ namespace StellarShipWeb.Models
 {
     public class BusquedaPaquete
     {
-        [Required]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         [Remote(action: "VerificarExisteTipoCuenta", controller: "TiposCuentas")]
         public long IdGuia { get; set; }
     }
